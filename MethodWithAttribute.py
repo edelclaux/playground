1bf33056783111885d8000b4095e1434ece632d3
Etienne Delclaux <etienne_delclaux@natural-solutions.eu>
2024-07-22 18:42:09 +0200

    feat: update

---
+++ MethodWithAttribute.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+
+class Module(ABC):
+    def method(self):
+        return "/import/module/process/upload"
+    method.label = "Importer des occurrences de taxons"
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from ModuleOcchab import ModuleOcchab
-from ModuleSynthese import ModuleSynthese
-from ModuleMetadata import ModuleMetadata
+# from ModuleOcchab import ModuleOcchab
+# from ModuleSynthese import ModuleSynthese
+# from ModuleMetadata import ModuleMetadata
 
-module_occhab = ModuleOcchab()
-print(f"{module_occhab.description()}\n")
+# module_occhab = ModuleOcchab()
+# print(f"{module_occhab.description()}\n")
 
-module_synthese = ModuleSynthese()
-print(f"{module_synthese.description()}\n")
+# module_synthese = ModuleSynthese()
+# print(f"{module_synthese.description()}\n")
 
-module_metadata = ModuleMetadata()
-print(f"{module_metadata.description()}\n")
+# module_metadata = ModuleMetadata()
+# print(f"{module_metadata.description()}\n")
+
+from MethodWithAttribute import Module
+module = Module()
+print(module.method())
+print(module.method.label)
+
+attr = getattr(module.method, "object_code", "ALL")
+print(attr)
